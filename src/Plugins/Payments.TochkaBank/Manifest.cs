03b0a51e95869c4cebefec549488120f078595af
Anton Kabaev <a.kabaev@mts.ai>
2024-06-06 19:32:12 +0300

    начало работ по плагину оплаты точка банка

---
+++ src/Plugins/Payments.TochkaBank/Manifest.cs
@@ -0,0 +1,11 @@
+using Grand.Infrastructure;
+using Grand.Infrastructure.Plugins;
+
+[assembly: PluginInfo(
+    FriendlyName = "Tochka Bank Payment Plugin",
+    Group = "Payments",
+    SystemName = "Payments.TochkaBank",
+    SupportedVersion = GrandVersion.SupportedPluginVersion,
+    Author = "medicsmarket.org",
+    Version = GrandVersion.SupportedPluginVersion + ".001"
+)]
---
+++ src/Plugins/Payments.TochkaBank/TochkaBankPlugin.cs
@@ -0,0 +1,30 @@
+using Grand.Infrastructure.Plugins;
+
+public class TochkaBankPlugin : BasePlugin
+    {
+        private readonly ITranslationService _translationService;
+        private readonly ILanguageService _languageService;
+
+        public TochkaBankPlugin(ITranslationService translationService,ILanguageService languageService)
+        {
+            _translationService = translationService;
+            _languageService = languageService;
+        }
+		
+        public override string ConfigurationUrl()
+        {
+            return "/Admin/TochkaBankPlugin/Configure";
+        }
+
+        public async override Task Install()
+        {
+            await this.AddOrUpdatePluginTranslateResource(_translationService,     _languageService, "Plugins.Payments.TochkaBank.Value", "Sample value");
+            await base.Install();
+        }
+
+        public async override Task Uninstall()
+        {
+            await this.DeletePluginTranslationResource(_translationService, _languageService, "Plugins.Payments.TochkaBank.Value");
+            await base.Uninstall();
+        }
+    }
